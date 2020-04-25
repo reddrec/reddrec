0e4e771d76c22612918669024bcd731b0d0e393d
Jessica Sanborn <jessica.sanborn10@gmail.com>
2020-04-24 18:02:15 -0600

    attempt to fetch data from server. Didnt work

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,47 +1,50 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css'
 
-function Search() {
-  return (
-    <form className="Search">
-      <label className="search-box">
-        <input className="search-input" id="search" type="text" placeholder="Username" />
-      </label>
-      {/* <input className="okButton" type="submit" value="Ok" /> */}
-    </form>
-  )
-}
+// function Search() {
+//   return (
+//     <form className="Search">
+//       <label className="search-box">
+//         <input className="search-input" id="search" type="text" placeholder="Username" />
+//       </label>
+//       {/* <input className="okButton" type="submit" value="Ok" /> */}
+//     </form>
+//   )
+// }
 
 function Recommendation(props){
   return (
     <div className="recommendation">
-      <h3>{"#" + props.score + ":"}</h3>
-      <h3 style={{paddingLeft:"10px"}}>{"r/" + props.subreddit}</h3>
+      <h3>{"r/" + props.subreddit}</h3>
+      <h4>{"Confidence: " + props.confidence}</h4>
     </div>
   )
 }
 
 function App() {
-  const [data, setData] = useState({ hits: [] })
-
-  let recommendations = [
-    {
-      subreddit: "animalcrossing",
-      score: "1"
-    },
-    {
-      subreddit: "nintendoswitch",
-      score: "2"
-    },
-    {
-      subreddit: "thesims",
-      score: "3"
-    }
-  ]
-
-  let recommendationComponents = recommendations.map((item, i) => 
-    <Recommendation key={i} {...item} />
+  const [data, setData] = useState(0)
+  const [query, setQuery] = useState('jess2187')
+  const [url, setUrl] = useState(
+    '/recommend/jess2187',
   )
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false)
+      setIsLoading(true)
+      try {
+        const result = await axios(url)
+        setData(result.data)
+      } catch (error) {
+        setIsError(true)
+      }
+      setIsLoading(false)
+    };
+    fetchData()
+  }, [url])
   
   return (
     <div className="App">
@@ -49,9 +52,40 @@ function App() {
         <h1>Reddit Recommender</h1>
         <p>Find new gaming subreddits!</p>
       </div>
-      <Search/>
+      <div className="Search">
+        <div className="search-box">
+          <input
+            className="search-input"
+            type="text"
+            value={query}
+            placeholder="Username"
+            onChange={event => setQuery(event.target.value)}
+          />
+        </div>
+        <input 
+          className="okButton" 
+          type="submit" 
+          value="Ok" 
+          onClick={() =>
+            setUrl(`/recommend/${query}`)
+          }/>
+      </div>
+
+      {isError && <div>Something went wrong ...</div>}
+      
+      { isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          {/* {data.recommendations.map(item => (
+            <Recommendation subreddit={item.subreddit} confidence={item.confidence} />
+          ))} */}
+          {console.log(data)}
+        </div>
+      )}
+
       {/* <img src='./loading.gif' className="App-logo" alt="logo" /> */}
-      {recommendationComponents}
+      {/* {recommendationComponents} */}
     </div>
   )
 }

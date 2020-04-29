d361e1854ddece4daf200902724026d635a2d487
Evan Ram <evan@evanram.com>
2020-04-29 16:58:59 -0600

    Create initial structure for seeding scripts
    Seeding scripts are ones that create startup dependency data that relies
    on data from an external API (Reddit).
    
    Currently stubbed out for the most part

--- datadeps/__main__.py
+++ datadeps/__main__.py
@@ -1,11 +1,11 @@
-import numpy as np
-import pandas as pd
+import pandas
 import os
 import sys
 import pickle
 
 OUTPUT_DIR = 'gen'
 SUBREDDITS_CSV = 'vg-subreddits.csv'
+MATRIX_CSV = 'seeding/out/matrix.csv'
 
 def main():
     rel = f'./{sys.argv[0]}'
@@ -24,6 +24,7 @@ def main():
         os.makedirs(output_dir)
 
     write(f'{output_dir}/subreddits.pickle', read_subreddits(f'{rel}/{SUBREDDITS_CSV}'))
+    write(f'{output_dir}/matrix.pickle', read_matrix(f'{rel}/{MATRIX_CSV}'))
 
     print('mkdatadeps: Done!')
 
@@ -31,12 +32,23 @@ def write(path, obj):
     with open(path, 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
+def assert_exists(path):
+    if not os.path.exists(path):
+        print(f'mkdatadeps: ERROR! Missing file "{path}"')
+        sys.exit(1)
+
 def read_subreddits(path):
-    df = pd.read_csv(path)
+    assert_exists(path)
+    df = pandas.read_csv(path)
 
     # Just get the subreddit column as a list of strings...
     # The `[0]` is a dirty hack and should be replaced
     return df[['subreddit']].values.reshape((1, -1)).tolist()[0]
+
+def read_matrix(path):
+    assert_exists(path)
+    df = pandas.read_csv(path)
+    return df.values
 
 if __name__ == '__main__':
     main()
---
+++ datadeps/seeding/README.txt
@@ -0,0 +1,10 @@
+This directory contains scripts that gather data for the utility matrix. They
+can take a while to run but can be restarted at different checkpoints as
+necessary.
+
+Output files are CSV format with no header row.
+
+This is separate from `../datadeps/` because scripts here contain many Reddit
+API requests that can take a while.
+
+For privacy reasons, do not commit any output data to git!
